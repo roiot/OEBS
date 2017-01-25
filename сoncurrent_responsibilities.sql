47f48ca30ee008b303e8276bf50f5b76be0c7368
skryabin_dg <denis.skryabin@eurochem.ru>
2017-01-25 19:47:42 +0300

    Trace, Reports Errors, Concurrent Responsibilities

---
+++ сoncurrent_responsibilities.sql
@@ -0,0 +1,5 @@
+select * from  fnd_concurrent_programs_vl where concurrent_program_name = 'XXOPM_B116_N'
+
+select * from   fnd_request_group_units where request_unit_id = 168484
+
+select * from fnd_responsibility_vl where request_group_id in (2129, 794, 2049) and responsibility_name like 'RUNEV%'
